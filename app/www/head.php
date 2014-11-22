9eb9d3fbf6c062176275e331b0738891fa5d7471
Nikolay Plekhanov <hidden>
2014-11-22 12:16:13 +0300

    optimize for iPad

--- app/www/head.php
+++ app/www/head.php
@@ -2,4 +2,13 @@
     <meta http-equiv="content-type" content="text/html;charset=utf8"/>
     <link href="common.css" rel="stylesheet" type="text/css"/>
     <script src="common.js" type="text/javascript"></script>
+
+    <meta name="viewport" content="width=device-width">
+
+    <script type="text/javascript">
+        if(navigator.userAgent.match(/iPad/i)) {
+            viewport = document.querySelector("meta[name=viewport]");
+            viewport.setAttribute('content', 'width=600');
+        }
+    </script>
 </head>
