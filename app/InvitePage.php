ba13cbb77eb6fb6525c8fbc957d8dc7de5ce96e1
Nikolay Plekhanov <hidden>
2014-11-22 04:43:34 +0300

    fix UUID generation

--- app/InvitePage.php
+++ app/InvitePage.php
@@ -12,8 +12,22 @@ class InvitePage extends AdminPage {
     }
 
     function create_invite($params) {
-        $this->key = preg_replace('/[-{}]/','', com_create_guid());
+        $this->key = preg_replace('/[-{}]/','', $this->create_guid());
         insert("insert into planner_invite (`key`, description) values ('$this->key','$params->description__sql')");
+    }
+
+    private function create_guid() {
+        mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+        $hyphen = chr(45);// "-"
+        $uuid = chr(123)// "{"
+            .substr($charid, 0, 8).$hyphen
+            .substr($charid, 8, 4).$hyphen
+            .substr($charid,12, 4).$hyphen
+            .substr($charid,16, 4).$hyphen
+            .substr($charid,20,12)
+            .chr(125);// "}"
+        return $uuid;
     }
 
     function cancel_invite($params) {
