977fbb915d22ab058c3b35bf58dbd3abba91cf28
Nikolay Plekhanov <hidden>
2014-11-22 14:07:06 +0300

    categories folding

--- app/templates/tasks.php
+++ app/templates/tasks.php
@@ -1,6 +1,6 @@
 <html>
 <? include '../www/head.php' ?>
-<body>
+<body onload="update_toggles()">
 <? include 'menu.php' ?>
 <?
 /** @var $this Tasks */
@@ -8,109 +8,112 @@
 <?
 if ($this->user) {
     if ($this->categories) foreach ($this->categories as $cat) {
-        ?><h4 class="clickable" onclick="toggle('add_task<?=$cat->id?>')"><?= esc($cat->title) ?></h4>
-
-        <form id="add_task<?=$cat->id?>" method="post" class="state-hidden">
-            <input type="hidden" name="method" value="add_task"/>
-            <input type="hidden" name="category_id" value="<?= esc($cat->id) ?>"/>
-            <label>
-                <textarea name="title"></textarea>
-            </label>
-            <input type="submit" value="Добавить Задачу"/>
-        </form>
-        <ul><?
-        $tasks = $this->tasks_by_category[$cat->id];
-        if ($tasks) {
-            foreach ($tasks as $task) {
-                ?><li><span class="clickable" onclick="toggle('task_controls<?=$task->id?>')"><?= esc($task->title) ?>
-                    <span class="time">(<?=esc($task->opened_at)?>)</span></span>
-                <div id="task_controls<?=$task->id?>" class="state-hidden">
-                    <div class="controlgroup">
-                        <form method="post">
-                            <input type="hidden" name="method" value="add_comment"/>
-                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                            <label>
-                                <textarea name="content"></textarea>
-                            </label>
-                            <input type="submit" value="Добавить Комментарий"/>
-                        </form>
-                    </div>
-                    <div class="controlgroup">
-                        <form method="post">
-                            <input type="hidden" name="method" value="move_task_up"/>
-                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                            <input type="submit" value="Поднять выше"/>
-                        </form>
-                        <form method="post">
-                            <input type="hidden" name="method" value="move_task_down"/>
-                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                            <input type="submit" value="Опустить ниже"/>
-                        </form>
-                    </div>
-                    <div class="controlgroup">
-                        <form method="post">
-                            <input type="hidden" name="method" value="full_move_task_up"/>
-                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                            <input type="submit" value="В самый верх"/>
-                        </form>
-                        <form method="post">
-                            <input type="hidden" name="method" value="full_move_task_down"/>
-                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                            <input type="submit" value="В самый низ"/>
-                        </form>
-                    </div>
-                    <div class="controlgroup">
-                        <?
-                        foreach ($this->categories as $innerCat) {
-                            if ($innerCat->id != $cat->id) {
-                                ?>
+        ?><h4 class="clickable" onclick="toggle('tasks<?=$cat->id?>')"><?= esc($cat->title) ?></h4>
+        <div id="tasks<?=$cat->id?>">
+            <form method="post" style="float: right">
+                <input type="hidden" name="method" value="add_task"/>
+                <input type="hidden" name="category_id" value="<?= esc($cat->id) ?>"/>
+                <label>
+                    <textarea name="title"></textarea>
+                </label>
+                <input type="submit" value="Добавить Задачу"/>
+            </form>
+            <ul><?
+                $tasks = $this->tasks_by_category[$cat->id];
+                if ($tasks) {
+                    foreach ($tasks as $task) {
+                        ?><li><span class="clickable" onclick="toggle('task_controls<?=$task->id?>')"><?= esc($task->title) ?>
+                            <span class="time">(<?=esc($task->opened_at)?>)</span></span>
+                        <div id="task_controls<?=$task->id?>" class="state-hidden">
+                            <div class="controlgroup">
                                 <form method="post">
-                                    <input type="hidden" name="category_id" value="<?=$innerCat->id?>"/>
-                                    <input type="hidden" name="method" value="change_category"/>
+                                    <input type="hidden" name="method" value="add_comment"/>
                                     <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
-                                    <input type="submit" value="В <?=esc($innerCat->title)?>"/>
+                                    <label>
+                                        <textarea name="content"></textarea>
+                                    </label>
+                                    <input type="submit" value="Добавить Комментарий"/>
                                 </form>
-                            <?
+                            </div>
+                            <div class="controlgroup">
+                                <form method="post">
+                                    <input type="hidden" name="method" value="move_task_up"/>
+                                    <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
+                                    <input type="submit" value="Поднять выше"/>
+                                </form>
+                                <form method="post">
+                                    <input type="hidden" name="method" value="move_task_down"/>
+                                    <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
+                                    <input type="submit" value="Опустить ниже"/>
+                                </form>
+                            </div>
+                            <div class="controlgroup">
+                                <form method="post">
+                                    <input type="hidden" name="method" value="full_move_task_up"/>
+                                    <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
+                                    <input type="submit" value="В самый верх"/>
+                                </form>
+                                <form method="post">
+                                    <input type="hidden" name="method" value="full_move_task_down"/>
+                                    <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
+                                    <input type="submit" value="В самый низ"/>
+                                </form>
+                            </div>
+                            <div class="controlgroup">
+                                <?
+                                foreach ($this->categories as $innerCat) {
+                                    if ($innerCat->id != $cat->id) {
+                                        ?>
+                                        <form method="post">
+                                            <input type="hidden" name="category_id" value="<?=$innerCat->id?>"/>
+                                            <input type="hidden" name="method" value="change_category"/>
+                                            <input type="hidden" name="task_id" value="<?= esc($task->id) ?>"/>
+                                            <input type="submit" value="В <?=esc($innerCat->title)?>"/>
+                                        </form>
+                                    <?
+                                    }
+                                }
+                                ?>
+                            </div class="controlgroup">
+                        </div>
+                        <?
+                        if ($comments = $this->comments_by_task[$task->id]) {
+
+                            ?><ul><?
+                            foreach ($comments as $comment) {
+                                ?><li><pre style="display: inline"><?=esc($comment->content)?></pre> <span class="time">(<?=esc($comment->posted_at)?>)</span></li><?
                             }
+                            ?></ul><?
                         }
                         ?>
-                    </div class="controlgroup">
-                </div>
-                <?
-                if ($comments = $this->comments_by_task[$task->id]) {
-
-                    ?><ul><?
-                    foreach ($comments as $comment) {
-                        ?><li><pre style="display: inline"><?=esc($comment->content)?></pre> <span class="time">(<?=esc($comment->posted_at)?>)</span></li><?
+                        </li><?
                     }
-                    ?></ul><?
                 }
                 ?>
-                </li><?
-            }
-        }
-        ?>
-        </ul>
+            </ul>
+        </div>
     <?
     }
     ?>
     <h4>История</h4>
-
     <?
     foreach ($this->events_by_day as $day => $events) {
 
-    ?><?=$day?><?
-        ?><ul><?
-        foreach ($events as $event) {
-            $s = esc($event->f);
-            for ($i = 1; $i <= 4; $i ++) {
-                $var = "a$i";
-                $val = esc($event->$var);
-                $s = str_replace("%$i", "<span class='cite$i'>$val</span>", $s);
+    ?>
+        <div>
+            <span class="clickable" onclick="toggle('history<?=$day?>')"><?=$day?></span><?
+            ?><ul id="history<?=$day?>"><?
+            foreach ($events as $event) {
+                $s = esc($event->f);
+                for ($i = 1; $i <= 4; $i ++) {
+                    $var = "a$i";
+                    $val = esc($event->$var);
+                    $s = str_replace("%$i", "<span class='cite$i'>$val</span>", $s);
+                }
+                ?><li><span class="time"><?= format_date($event->at, 'H:i:s') ?></span> <?= $s ?></li><?
             }
-            ?><li><span class="time"><?= format_date($event->at, 'H:i:s') ?></span> <?= $s ?></li><?
-        }
-        ?></ul><?
+            ?></ul>
+        </div><?
     }
 }
 ?>
--- app/www/common.js
+++ app/www/common.js
@@ -6,4 +6,46 @@ function toggle(q) {
     } else {
         o.className = 'state-hidden';
     }
+    var toggles = getToggles();
+    toggles[q] = o.className;
+    setToggles(toggles);
+}
+
+function update_toggles() {
+
+    var toggles = getToggles();
+
+    for (var key in toggles) {
+        var element = document.getElementById(key);
+        if (element) {
+            element.className = toggles[key];
+        }
+    }
+
+    if (!toggles) {
+        var expires = new Date( new Date().getTime() + 3000*24*60*60*1000 );
+        document.cookie = 'toggles=;expires='+expires.toUTCString();
+    }
+}
+
+function getToggles() {
+    var togglesC = getCookie('toggles');
+    if (!togglesC) {
+        return {};
+    }
+    return JSON.parse(togglesC);
+}
+
+function setToggles(toggles) {
+    var value = JSON.stringify(toggles);
+    var expires = new Date( new Date().getTime() + 3000*24*60*60*1000 );
+    document.cookie = 'toggles='+encodeURIComponent(value)+';expires='+expires.toUTCString();
+}
+
+// возвращает cookie с именем name, если есть, если нет, то undefined
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
